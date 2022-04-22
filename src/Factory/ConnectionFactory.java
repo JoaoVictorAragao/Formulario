754ab7a13a5790f849255c33fe38dc9a5eaba920
JoaoVictorAragao <jojoba2018cnx@gmail.com>
2022-04-21 22:57:06 -0300

    Conexão com o Google Cloud
    Foi feito o import de todo java.sql para que a conexão com o BD no google cloud fosse bem sucedida

--- src/Factory/ConnectionFactory.java
+++ src/Factory/ConnectionFactory.java
@@ -3,7 +3,8 @@ package Factory;
 
 // faz as importações de classes necessárias para o funcionamento do programa 
 import java.sql.Connection; 
-
+import java.sql.*;
+//IMPORTANTE!!!! o import java.sql.* foi necessário para que a conexão com o banco de dados que está no google clouds fosse bem sucedida
 // conexão SQL para Java 
 
 import java.sql.DriverManager; 
@@ -15,8 +16,9 @@ import java.sql.SQLException;
 public class ConnectionFactory {
      public Connection getConnection() {
 		 try {
+			
 			//Subistitua o caminho pelo caminho do seu banco de dados, e as informações de login e senha do banco também.
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/forms?useTimezone=true&serverTimezone=UTC","root","");
+			return DriverManager.getConnection("jdbc:mysql://34.95.199.28:3306/Forms","root","123");
 		 }         
 		 catch(SQLException excecao) {
 			throw new RuntimeException(excecao);
